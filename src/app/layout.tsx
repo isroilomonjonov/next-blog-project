14939aa8be60f43da028070dcb12e35c0252b552
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-05-21 13:17:27 +0500

    nprogress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 "use client";
 import "nprogress/nprogress.css";
+import NProgress from "nprogress";
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 import { Box } from "@mui/material";
 import {Roboto } from "next/font/google";
 import React from "react";
+import { Router } from "next/router";
 import "@/styles/global.css";
-import Router from "next/router";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -19,7 +20,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
   return (
     <html lang="en">
       <body className={`${roboto.className}`}>
